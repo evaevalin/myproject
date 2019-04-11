71b4374d79db27c895bcc5a414aa56156f7839e5
luli <evalin19920321@gmail.com>
2019-04-11 11:57:38 +0800

    java and Kotlin variable, data type

--- src/main/java/com/tom/Hello.java
+++ src/main/java/com/tom/Hello.java
@@ -5,6 +5,17 @@ public class Hello {
      //   System.out.println("hello world");\
         Person p = new Person();
         p.hello();
+        int age = 19;
+        Integer age2 = 19;
+        char c ='A';
+        Character c2 = 'A'
+        byte b = 120;
+        float weight = 66.f;
+        float height = 1.7f;
+        boolean enroll = true;
+        String name = "Tom";
+
     }
+
 }
 
